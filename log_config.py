cab6a1f56a4d91fbf03aab45cbf1760fcfbedead
AnnaIvanovaAnna <39418435+AnnaIvanovaAnna@users.noreply.github.com>
2018-06-02 20:06:42 +0300

    lesson 3

--- log_config.py
+++ log_config.py
@@ -1,11 +1,12 @@
 import logging 
 import os
-
-logging.basicConfig(
-    filename =os.path.join(r'C:\Users\пользователь\Desktop','information.log'),
-    format = "%(asctime)s %(levelname)-10s %(module)-10s %(funcName)-10s %(message)s",
-    level = logging.INFO
-)
+import logging.handlers
 
 
-
+file_path=os.path.join(r'C:\Users\пользователь\Desktop','information.log')
+format=logging.Formatter("%(asctime)s %(levelname)-10s %(module)-10s %(funcName)-10s %(message)s")
+server_logger = logging.getLogger('server')
+server_handler = logging.handlers.TimedRotatingFileHandler(r'C:\Users\пользователь\Desktop\Python 2\information.log','d')
+server_handler.setFormatter(format)
+server_logger.addHandler(server_handler)
+server_logger.setLevel(logging.INFO)
